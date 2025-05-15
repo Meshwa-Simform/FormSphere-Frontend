4636fa5cfd125653e7c30f9574e30b1cb06857db
Meshwa-Simform <meshwa.patel@simformsolutions.com>
2025-05-15 14:17:06 +0530

    implemented my forms and share form

---
+++ src/app/modules/my-forms/interface/formOutput.ts
@@ -0,0 +1,59 @@
+export interface FormOutput{
+    message: string,
+    form: Form[],
+}
+
+export interface FormOutputWithId{
+    message: string,
+    form: Form,
+}
+
+export interface Form{
+    id: string,
+    userId: string,
+    title: string,
+    description: string,
+    logoUrl: string|null,
+    isSinglePage: boolean,
+    noOfPages: number,
+    questions: Question[],
+    styling?: Styling,
+    privateSharingToken?: string,
+    conditionalLogic?: conditionalLogic,
+    createdAt: string,
+    updatedAt: string,
+}
+
+export interface Styling{
+    PageColor: string,
+    PageImage?: string,
+    formColor: string,
+    fontColor: string,
+    fontFamily: string,
+    fontSize: number,
+}
+
+export interface Question{
+    id: string,
+    formId: string,
+    validations: string[],
+    pageNumber: number,
+    questionType:string,
+    questionText: string,
+    questionOptions: string[],
+    questionAnswer?: string,
+    questionOrder: number,
+    isRequired: boolean,
+    isHidden: boolean,
+    conditionalLogic?: conditionalLogic[],
+    createdAt: string,
+    updatedAt: string,
+}
+
+export interface conditionalLogic{
+    id: string,
+    formId: string,
+    questionId: string,
+    condition_check: string,
+    action_questionId: string[]
+}
--- src/app/shared/components/formsnav/formsnav.component.ts
+++ src/app/shared/components/formsnav/formsnav.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from '../../material.module';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formsnav',
   // prettier-ignore
-  // eslint-disable-next-line 
-  standalone: false,
+   
+  standalone: true,
+  imports:[MaterialModule, RouterModule, CommonModule],
   templateUrl: './formsnav.component.html',
   styleUrl: './formsnav.component.css'
 })
-export class FormsnavComponent {
+export class FormsnavComponent implements OnInit {
+  formId: string | null = null;
 
+  constructor(private _route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Check if a formId is provided in the route
+    this.formId = this._route.snapshot.paramMap.get('formId');
+  }
 }
